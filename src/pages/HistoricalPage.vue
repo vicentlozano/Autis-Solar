f549eba7ab093e89ceb5af6710d3c82ab2f61949
vicentlozano <lozanodroid@gmail.com>
2024-11-24 22:02:37 +0100

    okhlcColor

--- src/pages/HistoricalPage.vue
+++ src/pages/HistoricalPage.vue
@@ -1,10 +1,34 @@
 <template>
-<h1>Historical Data</h1>
-
+  <h4>Historical Data Search</h4>
+  <FilterTab
+    :optionsSelectOne="teamOptions"
+    :titleSelectOne="teamTitle"
+    :optionsRangeInterval="rangeIntervals"
+    :titleRangeInterval="titleRange"
+    :rangeCalendar="isRangeCalendar"
+    :isSearch="false"
+  />
 </template>
 <script setup>
+import FilterTab from "src/components/FilterTab.vue";
+
+const teamOptions = ["Emet", "Sistema 1"];
+const teamTitle = "Geeneral";
+const rangeIntervals = [
+  "Today",
+  "Yesterday",
+  "This week",
+  "Last week",
+  "This month",
+  "Last Month",
+  "Custom",
+];
+const titleRange = "intervals";
+const isRangeCalendar = true;
 </script>
 
 <style scoped>
-
+h4 {
+  text-align: center;
+}
 </style>
